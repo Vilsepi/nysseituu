6a8744cba0515768bc9fc765566871867b65efb3
Ville Seppanen <ville.seppanen@gofore.com>
2016-12-28 16:07:19 +0200

    Add summary health

--- src/main.py
+++ src/main.py
@@ -5,13 +5,23 @@ import config
 import healthcheck
 import tweet
 
-status = [healthcheck.check_site(site) for site in config.sites_to_check]
-print status
+SERVICE_DOWN = "Nysseituu, voihan pahus! :("
+SERVICE_UP = "Nysset kulkee, ainakin kartalla. :)"
 
-api = tweet.get_twitter_api(False)
-tweeting_allowed = tweet.is_tweeting_allowed()
 
-if tweeting_allowed:
-    message = 'Kulkeekohan pukki nyssellä?'
-    status = api.PostUpdate(message)
-    print status
+def handler(event, context):
+    checks = [healthcheck.check_site(site) for site in config.sites_to_check]
+    all_ok = all([check["health"] == "UP" for check in checks])
+    result = {"service_is_up": all_ok, "healthchecks": checks}
+
+    message = SERVICE_UP if all_ok else SERVICE_DOWN
+    print result, message
+
+    if tweet.is_tweeting_allowed():
+        api = tweet.get_twitter_api()
+        status = api.PostUpdate(message)
+        print status
+
+
+if __name__ == "__main__":
+    handler({},{})

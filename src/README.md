b864e728a88bcb613ade23ee3c4d4ccd1ae139eb
Ville Seppanen <ville.seppanen@gofore.com>
2016-12-28 17:24:29 +0200

    Add AWS SAM cloudformation template and helper tool to prepackage deps (wtf, why do I need to do this myself?)

---
+++ src/README.md
@@ -0,0 +1,10 @@
+
+[AWS Blog: Simplified serverless application deployment](https://aws.amazon.com/blogs/compute/introducing-simplified-serverless-application-deplyoment-and-management/)
+
+## Bummer
+
+- Package does not support ignoring of files (includes all files in the current directory)
+- Package does not support requirements.txt (you have to download deps yourself)
+
+Serverless framework is going to [wait it out](https://github.com/serverless/serverless/issues/2867)
+
---
+++ src/tool.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+STACK_NAME="nysseituu"
+ARTIFACTORY_BUCKET="lambda-artifactory"
+
+function deps {
+  rm -rf ../build
+  mkdir -p ../build
+  pip install -r requirements.txt -t ../build/
+}
+
+function package {
+  cp -rf {tweet,healthcheck} ../build/
+  cp -f {*.py,sam.yml} ../build/
+  cd ../build
+  aws cloudformation package --template-file sam.yml --output-template-file sam.compiled.yml --s3-bucket $ARTIFACTORY_BUCKET
+}
+
+function deploy {
+  cd ../build
+  aws cloudformation deploy --template-file sam.compiled.yml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
+}
+
+function help_and_exit {
+  echo "Error: Unknown subcommand, read script source for help."
+  exit 1
+}
+
+if [ $# -lt 1 ]
+then
+  help_and_exit
+fi
+case "$1" in
+  deps)
+    deps
+    ;;
+  package)
+    package
+    ;;
+  deploy)
+    deploy
+    ;;
+  *)
+    help_and_exit
+esac
